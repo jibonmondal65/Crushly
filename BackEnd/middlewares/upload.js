d8dd49d14dfe56cbb73296647d87e0424d3b345f
Jibon Mondal <mondaljibon933@gmail.com>
2026-02-15 12:44:18 +0530

    Update upload.js

--- BackEnd/middlewares/upload.js
+++ BackEnd/middlewares/upload.js
@@ -1,25 +1,28 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadPath = "uploads/avatars";
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/avatars"); // folder
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueName =
       Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      uniqueName + path.extname(file.originalname)
-    );
+    cb(null, uniqueName + path.extname(file.originalname));
   },
 });
-
 
 const upload = multer({
   storage,
   limits: {
-    fileSize: 2 * 1024 * 1024, // 2MB
+    fileSize: 2 * 1024 * 1024,
   },
 });
 
